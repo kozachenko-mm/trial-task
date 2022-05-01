373fe2096c4e24ad044ff2947669ffcd8640d032
koodeau <koodeau@gmail.com>
2022-05-01 16:23:07 +0200

    init

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,7 +4,14 @@ const config: GatsbyConfig = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-graphcms',
+      options: {
+        endpoint: "https://api-ca-central-1.graphcms.com/v2/cl2kmw2o40ni501z46usg4te0/master",
+      },
+    },
+  ],
 }
 
 export default config
